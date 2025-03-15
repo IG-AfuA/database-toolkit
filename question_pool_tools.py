5ecdab86c57bd44e75a02f4bb51577cae86a8221
Pepe <pepe_ghb@hb9evt.ch>
2025-03-15 10:27:58 +0100

    defs resorted and beta-list enhanced
    nothing of importance :-)

--- question_pool_tools.py
+++ question_pool_tools.py
@@ -1,5 +1,18 @@
+# no IMPORT needed
 
+def print_arguments():
+    print("Possible command line arguments are:")
+    print("-?   : Is showing this overview of possible arguments")
+    print("-e06 : Export question pool year 2006 for Novice Licence from BNetzA Germany")
+    print("-a07 : Export question pool year 2007 for Advanced Licence from BNetzA Germany")
+    print("-e24 : Export question pool year 2024 for Novice Licence from BNetzA Germany")
+    print("-a24 : Export question pool year 2024 for Advanced Licence from BNetzA Germany")
+    print("And only for beta testing: either '-beta' or '-math'")
+    print("-beta : Only some typical examples from the selected question pool will be exported")
+    print("-math : Only questions containing LaTex code will be exported")
 
+def print_separation_line():
+    print("--------------------------------")
 
 # Dictionary of allowed command line arguments
 # Values are needed for:
@@ -7,10 +20,11 @@
 # -- info in the Excel in the field 'Ergänzung A'
 dict_arguments  = {
     "-e06":"DLE-2006",
-    "-a07":"DLE-2007",
+    "-a07":"DLA-2007",
     "-e24":"DLE-2024",
     "-a24":"DLA-2024",
-    "-beta":"BETA"
+    "-beta":"ONLY-FOR-BETA-TESTING",
+    "-math":"ONLY-FOR-BETA-TESTING"
 }
 
 ACTIVE_POOL = ""
@@ -27,7 +41,22 @@ def check_active_pool(pool_list: list):
 
 def check_arguments(arguments):
     count_error = 0
-    if '-beta' in arguments:
+
+    if '-?' in arguments:
+        print_separation_line()
+        print_arguments()
+        print_separation_line()
+        exit()
+
+    if '-beta' in arguments and '-math' in arguments:
+        print_separation_line()
+        print("'-beta' and '-math' can not be used together")
+        print_separation_line()
+        print_arguments()
+        print_separation_line()
+        exit()
+
+    if '-beta' in arguments or '-math' in arguments:
         maximal_arg = 2
     else:
         maximal_arg = 1
@@ -67,19 +96,6 @@ def check_arguments(arguments):
         exit()
 
 
-def print_arguments():
-    print("Possible command line arguments are:")
-    print("-e06 : Export question pool year 2006 for Novice Licence from BNetzA Germany")
-    print("-a07 : Export question pool year 2007 for Advanced Licence from BNetzA Germany")
-    print("-e24 : Export question pool year 2024 for Novice Licence from BNetzA Germany")
-    print("-a24 : Export question pool year 2024 for Advanced Licence from BNetzA Germany")
-    print("-beta : Only some typical examples from the selected question pool will be exported")
-
-
-def print_separation_line():
-    print("--------------------------------")
-
-
 def beta_test_exam_questions(question : str):
 
     if check_active_pool(['-e06']):
@@ -87,7 +103,7 @@ def beta_test_exam_questions(question : str):
             "TA104",    # answer with superscript
             "TB105",    # answers with very long texts
             "TB604",    # answers with number with kommas
-            "TB610",    # question with image in squre
+            "TB610",    # question with image in square
             "TB902",    # answers with math term
             "TC105",    # answer with image in portrait
             "TC106",    # answers are images in portrait
@@ -101,7 +117,7 @@ def beta_test_exam_questions(question : str):
             "TF205",    # question with a lage image
             "TH201",    # answers with lamda / 2 term
             "TH205",    # answers with %
-            "TI204",    # answer with 4 digit numbers
+            "TI204",    # answer with 4-digit numbers
             "TL201",    # answer with long math term
             "TL202",    # answer with long math term
             "TL302"    # answer with superscript
@@ -127,12 +143,12 @@ def beta_test_exam_questions(question : str):
             "TB910",    # answers with long math terms
             "TB923",    # answers with frac math terms
             "TC202",    # answers with images
-            "TC206",    # answers with mycro
+            "TC206",    # answers with micro
             "TC507",    # answers with image between text
             "TC511",    # big image in question; answers with 1,2,3
             "TC512",    # big image in question; answers with 1,2,3
             "TC513",    # big image in question; answers with 1,2,3
-            "TC115",    # answers with big imaages
+            "TC115",    # answers with big images
             "TC526",    # images in question and answer
             "TC601",    # answers with 1,2
             "TC619",    # big image in question
@@ -152,8 +168,80 @@ def beta_test_exam_questions(question : str):
         ]
         return question in selection
 
+    elif check_active_pool(['-e24']):
+        selection = [
+            "EA108",    # answers with superscript
+            "EA112",    # answer with omega sign
+            "EA113",    # answer with micro sign
+            "EA116",    # answers with long numbers
+            "EB203",    # question with small image; answers with /
+            "EB303",    # answers with °
+            "EB305",    # one answer with lamda sign
+            "EB310",    # question with big image im portrait format
+            "EB411",    # question with big image
+            "EB505",    # answers with square root math term
+            "EB506",    # answers with math term
+            "EC110",    # answers with small images
+            "EC113",    # answer with numbers and omega sign
+            "EC115",    # question with small image
+            "EC301",    # answers with big images
+            "EC306",    # question with 2 images, different size
+            "EC506",    # question with big image
+            "EC512",    # answers with images between text
+            "EC516",    # answers with omega sign and /
+            "EC520",    # big images as answers
+            "EC609",    # answers with 1,2,3
+            "ED101",    # answers with frac math term
+            "ED120",    # answers with micro signs
+            "EE101",    # answer with images im landscape format
+            "EE203",
+            "EE403",    # question with 'sz'; answer with square root
+            "EF309",    # question with big image
+            "EG102",    # answers with lamda sign
+            "EG111",    # image in question should be one big image
+            "EG502"    # answers with long math terms
+        ]
+        return question in selection
+
+    elif check_active_pool(['-a24']):
+        selection = [
+            "AB103",    # Testen frac --> dfrag #FIXME
+            "AB104",    # answers with long texts
+            "AB208",    # answers with omega signs
+            "AC304",    # 'kOhm.' in question
+            "AC405",    # images in question and answers #FIXME Bild von Frage fehlt
+            "AC406",    # images in question and answers #FIXME Bild von Frage fehlt
+            "AC515",    # 'kOhm' in answers #FIXME
+            "AC522",    # 'kOhm ' und 'kOhm;' in question #FIXME
+            "AC523",    # 'm\Omega' in question #FIXME
+            "AD109",    # question with image in landscape format #FIXME 'kOhm' in Frage
+            "AD110",    # ERROR missing subscript in answers # FIXME
+            "AD111",    # ERROR missing subscript in answers # FIXME
+            "AD113",    # kOhm; und kOhm. # FIXME
+            "AD114",    # question with image in portrait format
+            "AD305",    # square size images in all answers
+            "AD307",    # images in landscape format in all answers
+            "AD308",    # images in question and answers #FIXME
+            "AD402",    # questions and answers with v_U #FIXME utf/LaTex-Combo
+            "AD406",    # images in question and answers #FIXME Bild von Frage fehlt
+            "AD408",    # images in question and answers #FIXME Bild von Frage fehlt
+            "AD410",
+            "AD416",    # ERROR missing subscript in answers # FIXME
+            "AF120",    # question with large size image in landscape format
+            "AF210",    # question with large size image in landscape format
+            "AF314",    # ERROR answers #FIXME
+            "AF426",    # question with big square size image
+            "AG502",    # question with long math term #FIXME
+            "AH102",    # answers with long texts
+            "AH204",    # question with foF2, #FIXME
+            "AH206",    # answers with f_opt, f_krit, foF2, #FIXME
+            "AK103"     # testen frac --> dfrac
+        ]
+        return question in selection
+
     else:
         print_separation_line()
-        print("Pool unbekannt in question_pool_tools, def beta_test...")
+        print("ERROR in modul question_pool_tools.py")
+        print("Question pool unknown in def beta_test_exam_question()")
         print_separation_line()
-        return True     # every question is accepted
+        return True     # True = every question is accepted
