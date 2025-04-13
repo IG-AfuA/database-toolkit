2537d4d97958578d09425b9faa3b56e14d98e2ff
Pepe <pepe_ghb@hb9evt.ch>
2025-04-14 01:46:06 +0200

    Category names for all question pools in one Excel
    Category names for all question pools in one Excel file (for each question pool one worksheet).
    And not separate excel files for each question pool.

--- toolkit_categories.py
+++ toolkit_categories.py
@@ -30,11 +30,11 @@ def read_new_categories_from_xls(xls_path_name, xls_file_name, xls_sheet_name) -
 
     # Read the needed rows in the Excel;
     # - there is a title row. So data starts in row 2 --> 'min_row'
-    # - Needed data is in column 0 and column 2 --> 'min_col' and 'max_col'
-    for row in worksheet.iter_rows(min_row=2, max_row=worksheet.max_row, min_col=1, max_col=3, values_only=True):
+    # - Needed data is in column 1 and column 3 --> 'min_col' and 'max_col'
+    for row in worksheet.iter_rows(min_row=2, max_row=worksheet.max_row, min_col=1, max_col=4, values_only=True):
 
-        key = row[0]    # in column 0 is the code we use as key
-        value = row[2]  # in column 2 is the category name
+        key = row[1]    # in column 1 is the code we use as key
+        value = row[3]  # in column 3 is the category name
 
         if key is not None:
             if key not in check_duplicates:
@@ -44,9 +44,16 @@ def read_new_categories_from_xls(xls_path_name, xls_file_name, xls_sheet_name) -
                 check_duplicates.append(key)
             else:
                 #  Exit wih error message because key exists twice
-                dev_print("Key = '" + key + "' exists twice:")
-                dev_print("--> First  value = '" + result_dict.get(key) + "'")
-                dev_print("--> Second value = '" + value + "'")
+                dev_print("Key = '" + key + "' exists twice:  <--- check this key")
+                if result_dict.get(key) is not None:
+                    dev_print("--> First  value = '" + result_dict.get(key) + "'")
+                else:
+                    dev_print("--> First  value = None")
+                if value is not None:
+                    dev_print("--> Second value = '" + value + "'")
+                else:
+                    dev_print("--> Second value = None")
+                dev_print("worksheet.max_row = " + str(worksheet.max_row) + " <--- check, if correct")
                 exit_with_line_info("Duplicates in column 0 in worksheet '" + xls_sheet_name + "' in file '" + xls_file_name + "'")
         else:
             if value is not None:
